f7b9296a549e1d61aba700fca1f2dfeae547e089
cykreet <cykreet@gmail.com>
2024-06-15 12:59:15 +0200

    fix: textured button widget textures

--- src/main/java/ezzenix/smoothgui/mixin/TexturedButtonWidgetMixin.java
+++ src/main/java/ezzenix/smoothgui/mixin/TexturedButtonWidgetMixin.java
@@ -1,40 +1,39 @@
 package ezzenix.smoothgui.mixin;
 
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import ezzenix.smoothgui.SmoothGui;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ButtonTextures;
-import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
-import net.minecraft.client.gui.screen.recipebook.RecipeGroupButtonWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 @Mixin(TexturedButtonWidget.class)
 public class TexturedButtonWidgetMixin {
     @Final
     @Shadow
-    protected ButtonTextures textures;
-
-    @Inject(method="renderWidget", at=@At("HEAD"))
+    protected Identifier texture;
+    private static final Identifier RECIPE_BUTTON_TEXTURE = new Identifier("textures/gui/recipe_button.png");
+    
+    @Inject(method="renderButton", at=@At("HEAD"))
     private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (SmoothGui.isInMenu()) return;
         //TexturedButtonWidget t = (TexturedButtonWidget)(Object)this;
-        if (textures == RecipeBookWidget.BUTTON_TEXTURES) {
+        if (this.texture == RECIPE_BUTTON_TEXTURE) {
             context.getMatrices().translate(0.0, -SmoothGui.getOffsetY(), 0.0);
         }
     }
-    @Inject(method="renderWidget", at=@At("TAIL"))
+    @Inject(method="renderButton", at=@At("TAIL"))
     private void onRenderEnd(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (SmoothGui.isInMenu()) return;
-        if (textures == RecipeBookWidget.BUTTON_TEXTURES) {
+        if (this.texture == RECIPE_BUTTON_TEXTURE) {
             context.getMatrices().translate(0.0, SmoothGui.getOffsetY(), 0.0);
         }
     }
